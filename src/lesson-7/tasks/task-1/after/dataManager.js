ff6c75f67fd084bec455ec3b8c1432305dfee84d
Volodymyr Mordas <vmordas@alteryx.com>
2017-11-29 11:39:37 +0200

    reorganize

--- src/lesson-7/tasks/task-1/after/dataManager.js
+++ src/lesson-7/tasks/task-1/after/dataManager.js
@@ -1,28 +1,50 @@
-const map = new Map();
+export const DataManager = function(){
+    const map = new Map();
+    return {
+        add(entity) {
+            map.set(map.size, entity);
+        },
+        getEntities() {
+            return map.values();
+        },
+        getCount() {
+            return map.size;
+        },
+        getEntityById(id) {
+            return map.has(id) ? map.get(id) : null;
+        },
+        getFirstEntity() {
+            return map.size === 0 ? null : [...map.values()][0];
+        },
+        getLastEntity(){
+            return map.size === 0 ? null : [...map.values()][map.size - 1];
+        },
+        filter(callback){
+            return [...map.values()].filter((value) => callback(value));
+        }
+    }
+};
+// DataManager.prototype.add = function(entity) {
+//     map.set(map.size, entity);
+// };
+// DataManager.prototype.getEntities = function() {
+//     return map.values();
+// };
+// DataManager.prototype.getCount = function() {
+//     return map.size;
+// };
+// DataManager.prototype.getEntityById = function(id) {
+//     return map.has(id) ? map.get(id) : null;
+// };
+// DataManager.prototype.getFirstEntity = function() {
+//     return map.size === 0 ? null : [...map.values()][0];
+// };
+// DataManager.prototype.getLastEntity = function(){
+//     return map.size === 0 ? null : [...map.values()][map.size - 1];
+// };
+// DataManager.prototype.filter = function(callback){
+//     return [...map.values()].filter((value) => callback(value));
+// };
 
-const DataManager = function(){};
 
-DataManager.prototype.add = function(entity) {
-    map.set(map.size, entity);
-};
-DataManager.prototype.getEntities = function() {
-    return map.values();
-};
-DataManager.prototype.getCount = function() {
-    return map.size;
-};
-DataManager.prototype.getEntityById = function(id) {
-    return map.has(id) ? map.get(id) : null;
-};
-DataManager.prototype.getFirstEntity = function() {
-    return map.size === 0 ? null : [...map.values()][0];
-};
-DataManager.prototype.getLastEntity = function(){
-    return map.size === 0 ? null : [...map.values()][map.size - 1];
-};
-DataManager.prototype.filter = function(callback){
-    return [...map.values()].filter((value) => callback(value));
-};
-
-
-export {DataManager};
+// export {DataManager};
